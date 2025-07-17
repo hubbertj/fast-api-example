6ad99bbf186a35488e553d02c26bc3cd1a3edf12
Jerum Hubbert <hubbertj@gmail.com>
2025-07-17 09:33:13 -0700

    turning application into a milti tenet

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from controllers.book_controller import router as book_router
 from controllers.home_controller import router as home_router
 
-app = FastAPI()
+booksApp = FastAPI()
 
 # Include routers for modular route management
-app.include_router(book_router)
-app.include_router(home_router)
+booksApp.include_router(book_router)
+booksApp.include_router(home_router)
